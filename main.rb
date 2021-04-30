872eea2d8c13bd6afe007fe51a132cbe7ae0b432
Douglas Amport <douglas.amport@gmail.com>
2021-04-30 12:19:11 -0400

    updated changes to main.rb

--- main.rb
+++ main.rb
@@ -19,21 +19,8 @@ require "./player.rb"
 END {
 
 puts "Hi, Let\'s play Tic-Tac-Toe in the console."
-<<<<<<< HEAD
 
 setup_players
-=======
-puts "\n"
-puts 'Enter a name for player one.'
-temp_name = gets.chomp
-puts "Welcome #{temp_name} would you like to be X's or 0's (Please enter x or o. X's go first)"
-temp_symbol = get_symbol
-player1 = Player.new(temp_name, temp_symbol)
-puts 'Enter a name for player two.'
-temp_name = gets.chomp
-temp_symbol = player1.symbol == 'X' ? 'O' : 'X'
-player2 = Player.new(temp_name, temp_symbol)
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
 
 game1 = Game.new
 }
@@ -46,11 +33,7 @@ game1 = Game.new
 
 class Game
   attr_reader :first_player, :second_player
-<<<<<<< HEAD
   attr_accessor :board_arr, :played_arr
-=======
-  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight, :nine
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
   # @@current_turn = @first_player
   def initialize
     @first_player = Player.players.find { |plyr| plyr.instance_variable_get(:@first)}
@@ -67,18 +50,13 @@ class Game
     #{@ln} 
     #{@board_arr[6]} | #{@board_arr[7]} | #{@board_arr[8]} "
     .lines.map { |str| str.strip.center(20)}.join("\n")
-<<<<<<< HEAD
     puts "This is your game board.  Each number represents a position for Tic-Tac-toe"
     self.print_status
 # binding.pry
-=======
-    self.print_status
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
     self.round
   end
 
   def round
-<<<<<<< HEAD
 # binding.pry
     if check_for_winner
 # binding.pry
@@ -96,29 +74,11 @@ class Game
       self.check_game_status(scenerios_array, self.first_player.name, self.first_player.symbol)
       self.round
     end
-=======
-    
-    puts "#{self.first_player.name} please select a number to place your X"
-    player_selection = self.get_selection
-    self.update_board(player_selection, self.first_player.symbol)
-#binding.pry
-#    self.check_game_status
-
-    puts "#{self.second_player.name} please select a number to place your O"
-    player_selection = self.get_selection
-    self.update_board(player_selection, self.second_player.symbol)
-    self.check_game_status
-    self.round
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
   end
 
   def get_selection
     selection = gets.chomp.to_i
-<<<<<<< HEAD
 # binding.pry
-=======
-  #binding.pry
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
     unless selection.to_s.length == 1 && selection.class == Integer && selection != 0
       puts "Invalid selection. Please enter a number 1 thru 9"
       self.get_selection
@@ -133,7 +93,6 @@ class Game
   
   end
 
-<<<<<<< HEAD
   def update_board(num, current_symbol) # maybe this could use an array
     
     self.board_arr[num - 1] = current_symbol
@@ -150,37 +109,6 @@ class Game
 
 # binding.pry
 
-=======
-  def update_board(num, current_symbol) #maybe this could use an array
-    case num
-      when 1
-        self.one = current_symbol
-      when 2
-        self.two = current_symbol
-      when 3
-        self.three = current_symbol
-      when 4
-        self.four = current_symbol
-      when 5
-        self.five = current_symbol
-      when 6
-        self.six = current_symbol
-      when 7
-        self.seven = current_symbol
-      when 8
-        self.eight = current_symbol
-      when 9
-        self.nine = current_symbol
-    end
-    @game_board = 
-    " #{@one} | #{@two} | #{@three} 
-    #{@ln}
-     #{@four} | #{@five} | #{@six} 
-    #{@ln} 
-    #{@seven} | #{@eight} | #{@nine} "
-    .lines.map { |str| str.strip.center(20)}.join("\n")
-# binding.pry
->>>>>>> f31dac60c0a346be20aa7e6e7cd5ccd553feb79c
     self.print_status
   end
 
