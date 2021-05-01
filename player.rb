cdfd22e67c1af04f9a658f610dd342d345ec77f8
Douglas Amport <douglas.amport@gmail.com>
2021-05-01 07:11:42 -0400

    refacor setup_players.rb

--- player.rb
+++ player.rb
@@ -1,16 +1,28 @@
 class Player
   attr_reader :name, :symbol
+  protected
+
   @@players = []
   @@used_symbols = []
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
-    @first = @symbol == "X"
+    @first = @symbol == 'X'
     @@players << self
   end
+
+  def self.whos_on_first
+    Player.players.find { |plyr| plyr.instance_variable_get(:@first)}
+  end
+
+  def self.whats_on_second
+    Player.players.find { |plyr| !plyr.instance_variable_get(:@first)}
+  end
+
   def self.players
     @@players
   end
+
   def self.used_symbols
     @@used_symbols
   end
--- setup_players.rb
+++ setup_players.rb
@@ -1,23 +1,23 @@
-require "./player.rb"
+require './player'
 
-def get_symbol 
+def find_symbol
   entry = gets.chomp.upcase
-  unless entry == "X" || entry == "O"
-    puts "That selection is invalid.  Please select the letter X or O."
-    get_symbol
+  unless %w[X O].include?(entry)
+    puts 'That selection is invalid.  Please select the letter X or O.'
+    entry = find_symbol
   end
   entry
 end
 
-def setup_players
-  puts 'Enter a name for the player.'
+def setup_players(int)
+  puts "Enter a name for player #{(int + 1).to_s}."
   temp_name = gets.chomp
   if Player.used_symbols.empty?
     puts "Welcome #{temp_name}. Would you like to be X's or O's (Please enter x or o. X's go first)"
-    temp_symbol = get_symbol
+    temp_symbol = find_symbol
     Player.used_symbols << temp_symbol
   else
-    temp_symbol = Player.used_symbols.include?("X") ? 'O' : 'X'
+    temp_symbol = Player.used_symbols.include?('X') ? 'O' : 'X'
   end
   Player.new(temp_name, temp_symbol)
 end
