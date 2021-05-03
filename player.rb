77c32682bb2c54e9b6393663fe6e21e276947c4b
Douglas Amport <douglas.amport@gmail.com>
2021-05-03 19:50:26 -0400

    merged player_startup.rb into player class

--- player.rb
+++ player.rb
@@ -14,6 +14,28 @@ class Player #:nodoc:
     Player.players << self
   end
 
+  def self.find_symbol
+    entry = gets.chomp.upcase
+    unless %w[X O].include?(entry)
+      puts 'That selection is invalid.  Please select the letter X or O.'
+      entry = find_symbol
+    end
+    entry
+  end
+
+  def self.setup_players(int)
+    puts "Enter a name for player #{int + 1}."
+    temp_name = gets.chomp
+    if Player.used_symbols.empty?
+      puts "Welcome #{temp_name}. Would you like to be X's or O's (Please enter x or o. X's go first)"
+      temp_symbol = Player.find_symbol
+      Player.used_symbols << temp_symbol
+    else
+      temp_symbol = Player.used_symbols.include?('X') ? 'O' : 'X'
+    end
+    Player.new(temp_name, temp_symbol)
+  end
+
   class << self
 
     def whos_on_first
